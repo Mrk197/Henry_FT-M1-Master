0c3ad14f5bb3e2174e444858792b927975617af6
Mrk197 <mirkaalamilla97@gmail.com>
2022-08-03 18:09:02 -0500

    Homework 1 V1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -15,15 +15,10 @@ function DecimalABinario(num) {
   // tu codigo aca
   var binario = "";
   var residuo = 0;
-  var bin = "";
   while(num != 0){
     residuo = num % 2;
+    binario = residuo + binario;
     num = Math.floor(num/2);
-    bin = bin + residuo;
-  }
-  console.log(bin);
-  for(let i=bin.length-1; i>=0; i--){
-    binario = binario + bin[i];
   }
   return (binario);
 }
