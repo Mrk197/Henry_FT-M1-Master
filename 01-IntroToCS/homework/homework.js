842777d60d09ecb8d72efe9450eccb57b2475eb7
Mrk197 <mirkaalamilla97@gmail.com>
2022-08-02 19:00:58 -0500

    homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,30 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let n = 0;
+  let sum = 0;
+  for(let i=num.length-1; i>=0; i--){
+    sum += (2**n) * num[i];
+    n++;
+  }
+  return(sum);
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var binario = "";
+  var residuo = 0;
+  var bin = "";
+  while(num != 0){
+    residuo = num % 2;
+    num = Math.floor(num/2);
+    bin = bin + residuo;
+  }
+  console.log(bin);
+  for(let i=bin.length-1; i>=0; i--){
+    binario = binario + bin[i];
+  }
+  return (binario);
 }
 
 
@@ -15,3 +33,7 @@ module.exports = {
   BinarioADecimal,
   DecimalABinario,
 }
+
+
+//NOTAS 
+//Diferencia entre ** y Math.pow()
