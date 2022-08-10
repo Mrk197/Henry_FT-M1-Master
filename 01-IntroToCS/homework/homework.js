2138968c9c3ca977957cd5e87bc0fb91ff5d1398
Mrk197 <mirkaalamilla97@gmail.com>
2022-08-10 15:39:20 -0500

    notas review

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -5,24 +5,25 @@ function BinarioADecimal(num) {
   let n = 0;
   let sum = 0;
   for(let i=num.length-1; i>=0; i--){
-    sum += (2**n) * num[i];
+    //sum += (2**n) * parseInt(num[i]); //se agrega parseInt para ser explicitos, aunque JS lo resuelva sin ponerlo
+    sum += (2**n) * (+num[i]); // + convierte a numero 
     n++;
   }
   return(sum);
 }
 
+console.log(BinarioADecimal('1011'));
+
+
 function DecimalABinario(num) {
   // tu codigo aca
   var binario = "";
-  var residuo = 0;
   while(num != 0){
-    residuo = num % 2;
-    binario = residuo + binario;
+    binario = (num % 2) + binario;
     num = Math.floor(num/2);
   }
   return (binario);
 }
-
 
 module.exports = {
   BinarioADecimal,
@@ -31,4 +32,6 @@ module.exports = {
 
 
 //NOTAS 
-//Diferencia entre ** y Math.pow()
+//Diferencia entre ** y Math.pow() -->>optimización
+//do While -->> ejecuto ... reviso y continuo?
+//while -->> reviso si continuo? ... ejecuto
