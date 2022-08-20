8e93d7011d37e4fb0879279deec8e038f91546ee
Mrk197 <87842935+Mrk197@users.noreply.github.com>
2022-08-19 19:31:53 -0500

    Tarea 06 V1

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,61 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(root) {
+  this.root = new Node(root);
+  this.size = 1;
+  var currentRoot = this.root;
+
+  this.size = function(){
+    return this.size;
+  }
+
+  this.insert = function(value){   
+    if(value != currentRoot.value){ //si no existe el valor
+      //determinar si va a la izq. o der.
+      if(value < currentRoot.value){//si es menor va a la izq.
+        if(!currentRoot.left){ //si el nodo esta vacio
+          let newValue = new Node(value); //crea el nuevo nodo 
+          currentRoot.left = newValue; //lo inserta
+        }
+        else{ //no esta vacio
+          currentRoot = currentRoot.left;
+          this.insert(value);
+        }
+      } 
+      else{ // si es mayor va a la der.
+        if(!currentRoot.rigth){ //si el nodo esta vacio 
+          let newValue = new Node(value); //crea el nuevo nodo 
+          currentRoot.rigth = newValue;
+        }
+        else{ //no esta vacio
+          currentRoot = currentRoot.rigth;
+          this.insert(value);
+        }
+      }  
+    }
+    //si es igual no hace nada
+    this.size++; //se incremente el tamaño 
+  }
+
+  this.contains = function(){
+
+  }
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function(){
+
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function(){
+  
+}
+
+function Node(data) {
+  this.value = data;
+  this.left = null;
+  this.rigth = null;
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
