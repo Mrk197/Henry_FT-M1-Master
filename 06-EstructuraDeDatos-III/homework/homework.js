46f6a3df13d4300cd05bf7f570be3ac232b4bfc0
Mrk197 <mirkaalamilla97@gmail.com>
2022-08-23 17:18:55 -0500

    Tarea 06 review

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -9,8 +9,6 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
 
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
-*/
-
 function BinarySearchTree(root) {
   this._length = 1;
   this.value = root;
@@ -28,76 +26,39 @@ function BinarySearchTree(root) {
           if(!this.left){ //si rama izq esta vacia 
            this.left = new BinarySearchTree(value); //crea el nuevo nodo
             this._length++;
-
           }
-          else{ //no esta vacio
-            this.left.insert(value);
-          }
+          else this.left.insert(value); //no esta vacio
         }
         else{ // si es mayor va a la der.
           if(!this.right){ //si rama der esta vacia 
             this.right = new BinarySearchTree(value); //crea el nuevo nodo 
             this._length++;
-
           }
-          else{ //no esta vacio
-            this.right.insert(value);
-          }
+          else this.right.insert(value); //no esta vacio
         }
       }
-
     //si es igual no hace nada
   }
 
   this.contains = function(value){
-    console.log("value",this.value,typeof(this.value));
-    //console.log(typeof(value));
-    if(this.value == value){ //el valor es igual a la raíz?
-      return true;
-    }
+    if(this.value == value) return true; //el valor es igual a la raíz?
     //es mayor o menor?
     else if(value < this.value){ //es menor
       //hay un valor en el nodo izquierdo?
-      if(this.left){ //si
-        console.log("si hay en izq");
-        return this.left.contains(value); //ver si esta el valor
-      }
-      else{ //no
-        return false;
-      }
+      if(this.left) return this.left.contains(value); //si, ver si esta el valor
+      else return false; //no
     }
     else{ //es mayor
       //hay un valor en el nodo derecho?
-      if(this.right){ //si
-        return this.right.contains(value); //ver si esta el valor
-      }
-      else{ //no
-        return false;
-      }
+      if(this.right) return this.right.contains(value); //si, ver si esta el valor
+      else  return false; //no
     }
   }
+  
 } 
 
-BinarySearchTree.prototype.depthFirstForEach = function(order){
-  if(order=="post-order"){
-    if(this.left){ //si hay izq
-     this.left.depthFirstForEach(order);
-    }
-    else if(this.right){ //si hay derecha
-      this.right.depthFirstForEach(order);
-    }
-    else{ //rai
-      console.log("VAL", this.value);
-      return this.arr.push(this.value);
-    }
-  }
-
-  else if(order=="pre-order"){
-
-  }
-  else{ //in-order
-
-  }
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
+  
 }
 
 BinarySearchTree.prototype.breadthFirstForEach = function(){
@@ -116,6 +77,91 @@ arbol.insert(1);
 console.log(arbol);
 console.log(arbol.right.value);
 console.log(arbol.depthFirstForEach("post-order"));
+  */
+
+function BinarySearchTree(root) {
+  this.value = root;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.size = function(){
+  if(this.value === null) return 0;
+  if(this.left === null && this.right === null) return 1;
+  if(this.left === null && this.right !== null) return 1 + this.right.size();
+  if(this.right === null && this.left !== null) return 1 + this.left.size();
+  return 1 + this.left.size() + this.right.size();
+}
+
+BinarySearchTree.prototype.insert = function(value){
+  //evaluar value
+  if(value > this.value){ //mayores a la der
+    //si el nodo esta libre
+    if(this.right === null) this.right = new BinarySearchTree(value);
+    else this.right.insert(value);
+  }
+  if(value < this.value){ //menor a la izq
+    //si el nodo esta libre
+    if(this.left === null) this.left = new BinarySearchTree(value);
+    else this.left.insert(value);
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value){
+  if(this.value === value) return true;
+  if(value > this.value){ //mayores a la der
+    if(this.right === null) return false;
+    return this.right.contains(value);
+  }
+  if(value < this.value){ //menor a la izq
+    if(this.left === null) return false;
+    return this.left.contains(value);
+  }
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
+  //post-order
+  if(order === "post-order"){
+    //CB left
+    if(this.left !== null) this.left.depthFirstForEach(cb, order);
+    //CB right
+    if(this.right !== null) this.right.depthFirstForEach(cb, order);
+    //CB en root
+    cb(this.value);
+  }
+  //pre-order
+  else if(order === "pre-order"){
+    //CB en root
+    cb(this.value);
+    //CB left
+    if(this.left !== null) this.left.depthFirstForEach(cb, order);
+    //CB right
+    if(this.right !== null) this.right.depthFirstForEach(cb, order);
+  }
+  //in-order
+  else{
+    //CB left
+    if(this.left !== null) this.left.depthFirstForEach(cb, order);
+    //CB en root
+    cb(this.value);
+    //CB right
+    if(this.right !== null) this.right.depthFirstForEach(cb, order);
+  }
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, queue = []){
+  //guarda lo que hay en la izq
+  if(this.left !== null) queue.push(this.left);
+  //guarda lo que hay en la der
+  if(this.right !== null) queue.push(this.right);
+
+  //ejecutar root
+  cb(this.value);
+  //si hay elementos en la cola 
+  if(queue.length > 0){
+    queue.shift().breadthFirstForEach(cb, queue);
+  }
+}
 
 
 // No modifiquen nada debajo de esta linea
