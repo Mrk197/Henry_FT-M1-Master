91a42c7ab1a573b8ce5a3fac5b864fd6c0e6d85c
Mrk197 <mirkaalamilla97@gmail.com>
2022-08-30 20:05:52 -0500

    Testing

--- EXTRA-Testing/demo/sum.js
+++ EXTRA-Testing/demo/sum.js
@@ -1,4 +1,6 @@
 function sum(a, b) {
+  if(typeof(a) !== "number") throw new TypeError;
+  if(typeof(b) !== "number") throw new TypeError;
   return a + b;
 }
 
--- EXTRA-Testing/demo/sum.test.js
+++ EXTRA-Testing/demo/sum.test.js
@@ -1,7 +1,7 @@
 const sum = require('./sum');
 
 // it === test
-xit('should return 8 if adding 3 and 5', () => {
+it('should return 8 if adding 3 and 5', () => {
   // console.log(expect(sum(3,5)));
   expect(sum(3, 5)).toBe(8);
 });
@@ -10,3 +10,10 @@ xit('should return 15 if adding 7 and 8', () => {
   // console.log(expect(sum(3,5)));
   expect(sum(7, 8)).toBe(15);
 });
+
+it('prueba 3', () =>{
+  expect(sum(-8,5)).toBe(-3);
+});
+
+
+// agregar x a it significa que no va a testear, va a hacer skip
