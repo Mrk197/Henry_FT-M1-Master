1105948ed119286c950b0abb50956fbffd5402b9
Mrk197 <mirkaalamilla97@gmail.com>
2022-08-30 20:09:26 -0500

    Tarea 08 Review

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -9,7 +9,7 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  if(array.length <= 1){
+  /*if(array.length <= 1){
     return array;
 
   }
@@ -39,14 +39,41 @@ function merge(left, right) {
       }
   }
   //regresar copia de nuevo arreglo y si quedaron elementos en subarrays
-  return [ ...arr, ...left, ...right ];
+  return [ ...arr, ...left, ...right ];*/
+
+  if(array.length <= 1) return array;
+
+  let pivot = array[Math.floor(Math.random() * array.length)];
+  let left = [];
+  let right = [];
+  let equal = [];
+
+  //recorrer arreglo
+  for(let i=0; i< array.length; i++){
+    if(array[i] < pivot){
+      left.push(array[i]);
+    }
+    else if(array[i] > pivot){
+      right.push(array[i]);
+    }
+    else{
+      equal.push(array[i]);
+    }
+  }
+
+  return quickSort(left).concat(equal).concat(quickSort(right));
+
 }
+
+
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  
+  /*
   const half =  Math.floor(array.length/2); //calcula la mitad
   
   // terminar hasta el se haya dividido al menor tamaño
@@ -56,8 +83,43 @@ function mergeSort(array) {
   
   const left = array.splice(0, half) //divide a la mitad 
   return merge(mergeSort(left),mergeSort(array)); //une elementos ordenados 
-  /*
    */
+  
+  if(array.length === 1) return array;
+  
+  let division = split(array); //[[left],[rigth]]
+  let left = division[0]; //[left]
+  let right = division[1]; //[rigth]
+
+  return paste(mergeSort(left), mergeSort(right));
+}
+
+//funcion que divide el arreglo 
+function split(array){
+  let middle = Math.floor(array.length/2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle)
+
+  return [left, right]; //[[left],[rigth]]
+}
+
+//función que ordena y une 
+function paste(left, right){
+  let array = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while(leftIndex < left.length && rightIndex < right.length){
+    if(left[leftIndex] < right[rightIndex]){
+      array.push(left[leftIndex]);
+      leftIndex++;
+    }
+    else{
+      array.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 
